1b2f9084aec70891288dd16d4e94a8bfbe8458ae
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-06-21 23:11:22 -0300

    feat: POST methods (sign-up/tweet) GET method (tweet)
    ----------------
    Feature details:
    
    (*) Created global list for users and tweets.
    
    (*) Status code reqs for HTTP methods:
        - 200: Send list of tweets (max: 10).
        - 201: Created username (sign-up) / Created tweet.
        - 400: Bad request (empty username/avatar/tweet).
        - 401: Unauthorized (Trying to send a tweet without user auth).

--- src/app.js
+++ src/app.js
@@ -3,12 +3,74 @@ import cors from "cors";
 
 const PORT = 5000;
 
+const users = [];
+const tweets = [];
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Ola, Mundo!");
+app.post("/sign-up", (req, res) => {
+
+    const { username, avatar } = req.body;
+
+    if (!username || !avatar) {
+        return res.status(400).send("Todos os campos são obrigatórios!.");
+    }
+
+    users.push({
+        username: username,
+        avatar: avatar
+    });
+
+    return res.status(201).send("OK");
+})
+
+app.post("/tweets", (req, res) => {
+
+    const { username, tweet } = req.body;
+
+    const isUserAuth = () => {
+
+        const user = users.find((user) => user.username === username);    
+        return (typeof(user) !== "undefined") ? true : false;
+    }
+
+    if (!isUserAuth()) {
+        return res.status(401).send("UNAUTHORIZED");
+    }
+    
+    if (!username || !tweet) {
+        return res.status(400).send("Todos os campos são obrigatórios!.");
+    }
+
+    tweets.push({
+        username: username,
+        tweet: tweet
+    })
+
+    return res.status(201).send("OK");
+})
+
+app.get("/tweets", (req, res) => {
+
+    const latestTweets = tweets.slice(-10);
+    const listOfTweets = latestTweets.map(latestTweet => {
+
+        const user = users.find((user) => (user.username === latestTweet.username));
+
+        const username = latestTweet.username;
+        const tweet = latestTweet.tweet;
+        const avatar = user.avatar;
+
+        return {
+            username: username,
+            tweet: tweet,
+            avatar: avatar
+        };
+    })
+
+    return res.status(200).send(listOfTweets);
 })
 
 app.listen(PORT);
